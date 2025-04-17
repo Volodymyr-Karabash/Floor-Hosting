9cc3157bdfb9920a269cbe1fb5d6374deff998a2
Volodymyr-Karabash <voca480@gmail.com>
2025-04-17 15:31:20 +0300

    All Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import FAQ from './pages/FAQ';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/faq" element={<FAQ />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
